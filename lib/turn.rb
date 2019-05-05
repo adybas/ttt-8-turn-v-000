0582fc69d70f28890a565926246e863186950a5e
Anna Dybas <a.m.dybas@gmail.com>
2019-05-05 00:22:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,10 +10,12 @@ def input_to_index(input)
   input = gets.strip
 end
 
-def valid_move?()
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index) ? true : false
 end
 
-def position_taken?()
+def position_taken?(board, index)
+  board[index] === "X" || board[index] ==="O" ? true : false
 end
 
 def move
