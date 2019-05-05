55690ba21f453c340b21e089af1011788aed37c9
Anna Dybas <a.m.dybas@gmail.com>
2019-05-05 00:20:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,13 @@
-def display_board()
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index()
+def input_to_index(input)
+  input = gets.strip
 end
 
 def valid_move?()
